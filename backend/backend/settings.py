39d39836f2caf28d158342b40cc4c58fc566a02f
hunderaweke <hunderaweke@gmail.com>
2023-10-30 15:11:28 +0300

    updated: blog files

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,8 +119,8 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 
 USE_TZ = True
-
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = ["http://"]
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
